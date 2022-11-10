81f51fc36274da01b9925198a601c981c721caba
ashu180674 <ashutripathi16.10.2001@gmail.com>
2022-11-10 23:56:28 +0530

    firstcommit

---
+++ home/views.py
@@ -0,0 +1,9 @@
+from django.shortcuts import render , HttpResponse
+
+# Create your views here.
+
+def index(request):
+    return HttpResponse('this is my webpage')
+
+def about(request) :
+    return HttpResponse('about this page')
