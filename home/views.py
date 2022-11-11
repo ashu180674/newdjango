eebcac0c7f9d744df36dee3325044d7160a7c618
ashu180674 <ashutripathi16.10.2001@gmail.com>
2022-11-11 13:04:23 +0530

    day2

--- home/views.py
+++ home/views.py
@@ -3,7 +3,7 @@ from django.shortcuts import render , HttpResponse
 # Create your views here.
 
 def index(request):
-    return HttpResponse('this is my webpage')
+     return render(request,'index.html')
 
 def about(request) :
     return HttpResponse('about this page')
